27b73bcdf0ab95c3a0c0e2355e1ca15b7f7f0033
Areeg Fahad <fahedareeg@gmail.com>
2021-02-25 06:24:37 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <header className="App-header">
           // {moment().now}
           {"Now"}
-          
+
         </header>
         <p className="App-intro">
           In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
